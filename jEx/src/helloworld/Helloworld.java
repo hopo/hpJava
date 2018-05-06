84772d55bd8222f8abdff1de7ee5d35c5f5eec5a
hopo <hpsight@gmail.com>
2018-05-06 23:59:50 +0000

    hpJava/

---
+++ jEx/src/helloworld/Helloworld.java
@@ -0,0 +1,10 @@
+package helloworld;
+
+public class Helloworld {
+	public static void main(String[] args) {
+
+        String java = "java";
+		System.out.println("Hello " + java + " World!");
+
+	}
+}
---
+++ jEx/src/sumtest/SumExam.java
@@ -0,0 +1,12 @@
+package sumtest;
+
+public class SumExam {
+  public static void main(String args[]) {
+    int a = 10, b = 20;
+    int sum = (a+b);
+
+    sum += sum;
+
+    System.out.println(sum);
+  }
+}
